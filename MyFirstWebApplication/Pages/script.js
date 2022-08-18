25ea83b75bf39102120cc9552ceecb0d260f56e5
User <User@DESKTOP-I3HOUE5>
2022-08-18 06:26:23 +0400

    Add project files.

---
+++ MyFirstWebApplication/Pages/script.js
@@ -0,0 +1,22 @@
+﻿fetch("user.json")
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		let place = document.querySelector("#myTable");
+		let out = "";
+		for (let data of user) {
+			out += `
+			<tr>
+				
+				<td>${data.name}</td>
+				<td>${data.age}</td>
+				<td>${data.gender}</td>
+
+             </tr>
+
+		`;
+		}
+
+		place.innerHTML = out;
+	});
